f6d9bb8f58c2377449c1997349917221c0e9d7f1
Heikki Saul <heikki.saul@gmail.com>
2017-03-30 00:30:53 +0300

    rfid WIP

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process
 import esteid_data as ed
+import rfid_data as rd
 
 def rfid_placeholder(code):
 
@@ -8,12 +9,12 @@ def rfid_placeholder(code):
 
 p1 = Process(target=rfid_placeholder,args=("11111151515",))
 #pool.apply(ed.commit_data(ed.parse(ed.get_data())))
-p2 = Process(target=ed.test_commit)
-p3 = Process(target=rfid_placeholder,args=("22551515",))
+#p2 = Process(target=ed.test_commit)
+p3 = Process(target=rd.get_data())
 p4 = Process(target=rfid_placeholder,args=("2584484415",))
 
 p1.start()
-p2.start()
+#p2.start()
 p3.start()
 p4.start()
 
---
+++ src/rfid_data.py
@@ -0,0 +1,8 @@
+def get_data():
+    keyboard_input = input("Insert RFID tag code")
+
+    print(keyboard_input)
+    return
+
+if __name__ == "__main__":
+    print(get_data())
