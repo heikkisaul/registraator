53c652dd48f69ee145451054994044e2463e1a3e
heikkisaul <heikki.saul@gmail.com>
2017-04-12 18:09:01 +0300

    Push from device

--- src/esteid_data.py
+++ src/esteid_data.py
@@ -5,6 +5,8 @@ import subprocess
 import datetime
 #import pymysql
 from vars import *
+import os
+import signal
 g_data_list=[]
 
 #reload(sys)
@@ -14,8 +16,9 @@ g_data_list=[]
 # After being called, waits for card insertion. When a proper card is inserted, reads the data on the card chip
 def get_data(reader = '0'):
     call_eidenv = subprocess.Popen(['eidenv', reader, '-w'],stdout=subprocess.PIPE)#,stderr=subprocess.STDOUT)
-    call_eidenv.wait()
+    call_eidenv.wait(2)
     eidenv_raw = call_eidenv.communicate()[0]
+    #call_eidenv.kill()
     return eidenv_raw.decode('latin-1')
 
 # Parses raw data from get_eid_data() into a list [surname,first name,ID code]
--- src/main.py
+++ src/main.py
@@ -1,22 +1,29 @@
 from multiprocessing import Process
 import esteid_data as ed
 import example as rd
+import os
+import sys
 
-def rfid_placeholder(code):
-
-    print(code)
-
-
-p1 = Process(target=rfid_placeholder,args=("11111151515",))
-#pool.apply(ed.commit_data(ed.parse(ed.get_data())))
 p2 = Process(target=ed.test_commit)
 p3 = Process(target=rd.mainhook)
-p4 = Process(target=rfid_placeholder,args=("2584484415",))
 
-p1.start()
-p2.start()
-p3.start()
-p4.start()
+try:
+    p2.start()
+    p3.start()
+    os.execv(sys.executable,['python3'] + sys.argv)
+except:
+    os.execv(sys.executable, ['python3'] + sys.argv)0858860661
+    
+#try:
+    #ed.test_commit()
+    #rd.mainhook()
+
+    #os.execv(sys.executable, ['python3'] + sys.argv)
+    #exit()
+#except:
+    #os.execv(sys.executable,['python3'] + sys.argv)
+    #exit()
+
 
 
 
