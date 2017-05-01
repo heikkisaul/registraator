64bcc54403d9e85ab453eef0de0247125a3f4898
Heikki Saul <heikki.saul@gmail.com>
2017-05-01 15:13:10 +0300

    SSH and pymysql tested

---
+++ src/mypysql_ssh_test.py
@@ -0,0 +1,17 @@
+import pymysql
+
+conn = pymysql.connect(host='127.0.0.1', port=36629, user='lrb_admin', passwd='oiurfmneroejf5v9', db='lect_reg_base')
+
+cur=conn.cursor()
+
+cur.execute("SELECT * FROM USER")
+
+print(cur.description)
+
+print()
+
+for row in cur:
+    print(row)
+
+cur.close()
+conn.close()
---
+++ src/sshtunnel_test.py
@@ -0,0 +1,14 @@
+from sshtunnel import SSHTunnelForwarder
+from time import sleep
+
+server = SSHTunnelForwarder('ims.ut.ee', ssh_username="lectbot", ssh_password="ZUEAxLBr2L", remote_bind_address=('127.0.0.1', 3306))
+
+server.start()
+
+print(server.local_bind_port)
+
+while True:
+    sleep(1)
+
+server.stop()
+print("CLOSED")
